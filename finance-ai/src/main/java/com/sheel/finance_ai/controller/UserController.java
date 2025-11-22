acff275047ec1f7a74ce27da822c8b2a9f367791
Sheel Patel <sheel.patel126@gmail.com>
2025-11-21 22:08:03 -0500

    Day 2 for finance AI Project

---
+++ finance-ai/src/main/java/com/sheel/finance_ai/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.sheel.finance_ai.controller;
+
+import com.sheel.finance_ai.model.User;
+import com.sheel.finance_ai.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return service.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+}
---
+++ finance-ai/src/main/java/com/sheel/finance_ai/service/UserService.java
@@ -0,0 +1,24 @@
+package com.sheel.finance_ai.service;
+
+import com.sheel.finance_ai.model.User;
+import com.sheel.finance_ai.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<User> getAllUsers() {
+        return repo.findAll();
+    }
+
+    public User saveUser(User user) {
+        return repo.save(user);
+    }
+}
